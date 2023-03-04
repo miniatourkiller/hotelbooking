99d3e77560216d2d3717c4c65e094358fdc4b23e
James Karu <104491698+miniatourkiller@users.noreply.github.com>
2023-03-04 09:44:03 +0300

    Add files via upload

--- BookingMain/src/main/resources/public/web/header.js
+++ BookingMain/src/main/resources/public/web/header.js
@@ -1,6 +1,21 @@
-var link = `<li><a href="posting.html">All Rooms</a></li>`
-$(window).load(function(){
-    if(document.cookie != null){
-        $('.nav-links').append(link)
+var linkage = `<li><a href="posting.html">Hosting rooms</a></li>
+<li><a id="logout">logout</a></li>`
+window.addEventListener('load', function(){
+	 if(document.cookie.length>0 && this.document.cookie!="myid"){
+        $('.nav-links').append(linkage)
     }
+    $('#logout').click((e)=>{
+	e.preventDefault()
+	$.ajax({
+		url:"http://localhost:5070/logout",
+		success: function(data){
+			if(data == "done"){
+				document.cookie = "myid",+" ",-1;
+                if(document.cookie == "myid"){
+                    window.location.reload()
+                }
+			}
+		}
+	})
+})
 })
